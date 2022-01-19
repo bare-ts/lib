835f70bf70eec582dd0aab66a06c5cedb3a1f0b8
Victorien Elvinger <victorien@elvinger.fr>
2022-01-19 23:41:23 +0100

    chore: specify the ES version to emit (ES2019)

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,6 +8,7 @@ npx esbuild src/index.ts src/util/assert.ts \
     --bundle \
     --platform='node' --main-fields='module' \
     --format='cjs' \
+    --target='es2019' \
     --outdir='dist' \
     --out-extension:.js=.cjs \
     --log-level='warning'
@@ -18,6 +19,7 @@ npx esbuild src/**/*.ts src/*.ts \
     --external:'*.js' \
     --platform='node' --main-fields='module' \
     --format='esm' \
+    --target='es2019' \
     --outdir='dist' \
     --sourcemap \
     --log-level='warning'
